e5518eb8f52c5e1f95720746b9b2eb05f6cb8487
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-05-07 13:49:59 +0200

    Commit inicial

---
+++ js/audio.js
@@ -0,0 +1,33 @@
+// Rebote pared ─────────────────────────────────────────────────────────────
+function audioPared() {
+    var audio = new Audio("audio/rebotePared.mp3");
+    audio.play();
+}
+
+// Rebote jugador ─────────────────────────────────────────────────────────────
+function audioPlayer() {
+    var audio = new Audio("audio/rebotePlayer.wav");
+    audio.play();
+}
+
+// Game Over ─────────────────────────────────────────────────────────────
+function audioGameOver() {
+    var audio = new Audio("audio/gameOver.wav");
+    audio.play();
+}
+
+// Ladrillo ─────────────────────────────────────────────────────────────
+function audioLadrillo() {
+    var audio = new Audio("audio/ladrillo.wav");
+    audio.play();
+}
+// Ladrillo ─────────────────────────────────────────────────────────────
+function audioNuevoNivel() {
+    var audio = new Audio("audio/nuevoNivel.mp3");
+    audio.play();
+}
+// Ladrillo ─────────────────────────────────────────────────────────────
+function audioMusica() {
+    var audio = new Audio("audio/musica.mp3");
+    audio.play();
+}
